e6aeaeb6eb64526dccbb44ca0b545f0e2ca1b94e
Oranganza <kadamkajal62@gmail.com>
2025-06-13 14:18:41 +0530

    Tetsts

---
+++ Java13X/src/ex_03_literal/lab0035_Non_primitive.java
@@ -0,0 +1,10 @@
+package ex_03_literal;
+
+public class lab0035_Non_primitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java13X/src/ex_03_literal/table.java
@@ -0,0 +1,30 @@
+package ex_03_literal;
+
+public class table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 9;
+		System.out.printf("%d * %d = %d", 9, 1, 9);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 2, 18);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 3, 27);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 4, 36);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 5, 45);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 6, 54);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 7, 63);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 8, 72);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 9, 81);
+		System.out.println();
+		System.out.printf("%d * %d = %d", 9, 10, 90);
+	}
+
+}
